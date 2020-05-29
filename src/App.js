3ed7d15593b29ddca62bf486122644703a614c18
Huw-man <hu.newman@berkeley.edu>
2020-05-28 20:38:44 -0700

    loading models

--- src/App.js
+++ src/App.js
@@ -2,40 +2,35 @@ import React, {Component} from 'react';
 import ReactDom from "react-dom";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 const style = {
   height: '100vh'
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.loader = new GLTFLoader();
+    this.dracoLoader = new DRACOLoader();
+    this.dracoLoader.setDecoderPath('three/examples/js/libs/draco/');
+    this.loader.setDRACOLoader(this.dracoLoader);
+  }
+
   componentDidMount() {
     this.sceneSetup();
     this.addCustomSceneObjects();
+    this.loader.load(
+      './resources/models/Tugboat.gltf', 
+      this.loadModels, 
+      (xhr)=> {
+        console.log( xhr.loaded, xhr.total);
+      },
+      (error) => {console.log("model loading error");
+      });
     this.startAnimationLoop();
     window.addEventListener("resize", this.handleWindowResize);
   }
@@ -52,7 +47,7 @@ class App extends Component {
     // get container dimensions and use them for scene sizing
     const width = this.el.clientWidth;
     const height = this.el.clientHeight;
-    console.log(height)
+    
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
       75, // fov = field of view
@@ -60,7 +55,7 @@ class App extends Component {
       0.1, // near plane
       1000 // far plane
     );
-    this.camera.position.z = 5; // is used here to set some distance from a cube that is located at z = 0
+    this.camera.position.set(5, 10, 5); // is used here to set some distance from a cube that is located at z = 0
     // OrbitControls allow a camera to orbit around the object
     // https://threejs.org/docs/#examples/controls/OrbitControls
     this.controls = new OrbitControls(this.camera, this.el);
@@ -73,33 +68,41 @@ class App extends Component {
   // Code below is taken from Three.js BoxGeometry example
   // https://threejs.org/docs/#api/en/geometries/BoxGeometry
   addCustomSceneObjects = () => {
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    const material = new THREE.MeshPhongMaterial({
-      color: 0x156289,
-      emissive: 0x072534,
-      side: THREE.DoubleSide,
-      flatShading: true
-    });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
+    /*
+      Models
+    */
+    const planeSize = 1000;
+    const seaColor = 0x4db8ff
+    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshLambertMaterial({color: seaColor});
+    const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+    planeMesh.rotateX(-Math.PI / 2.0);
+    this.scene.add(planeMesh);
+    // const geometry = new THREE.BoxGeometry(2, 2, 2);
+    // const material = new THREE.MeshPhongMaterial({
+    //   color: 0x156289,
+    //   emissive: 0x072534,
+    //   side: THREE.DoubleSide,
+    //   flatShading: true
+    // });
+    // this.cube = new THREE.Mesh(geometry, material);
+    // this.scene.add(this.cube);
 
-    const lights = [];
-    lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
-
-    lights[0].position.set(0, 200, 0);
-    lights[1].position.set(100, 200, 100);
-    lights[2].position.set(-100, -200, -100);
-
-    this.scene.add(lights[0]);
-    this.scene.add(lights[1]);
-    this.scene.add(lights[2]);
+    /*
+      Lights
+    */
+    const skyColor = 0x87CEFA;
+    const hemiLight = new THREE.HemisphereLight(skyColor, seaColor, 1);
+    this.scene.add(hemiLight);
   };
 
+  loadModels = (gltf) => {
+    this.scene.add(gltf.scene);
+  }
+
   startAnimationLoop = () => {
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    // this.cube.rotation.x += 0.01;
+    // this.cube.rotation.y += 0.01;
 
     this.renderer.render(this.scene, this.camera);
 
@@ -126,6 +129,7 @@ class App extends Component {
   }
 }
 
+/*
 class Container extends React.Component {
   state = { isMounted: true };
 
@@ -146,8 +150,6 @@ class Container extends React.Component {
     );
   }
 }
+*/
 
 export default App;
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<Container />, rootElement);
